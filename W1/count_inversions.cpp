5649de2513e2d99df175cbd1689b0f8ec7a5c68e
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 15:50:27 +0530

    Update count_inversions.cpp

--- W1/count_inversions.cpp
+++ W1/count_inversions.cpp
@@ -1,29 +1,23 @@
-/*
-    Problem Link: https://practice.geeksforgeeks.org/problems/inversion-of-array-1587115620/1
-*/
-
 int merge(vector<int> &arr, int start, int mid, int end) {
-    int n1 = mid - start + 1; // mid is included
+    int n1 = mid - start + 1; 
     int n2 = end - mid;
-    // create 2 arrays for the left and right part
     int left[n1], right[n2];
     for(int i = 0; i < n1; i++)
         left[i] = arr[start + i];
     for(int i = 0; i < n2; i++)
         right[i] = arr[mid + i + 1];
-    // 3 pointers
+   
     int i = 0, j = 0, k = start;
     int count = 0;
     while(i < n1 && j < n2) {
         if(left[i] <= right[j])
             arr[k++] = left[i++];
-        else {
-            // count inversions
+        else 
+        {
             count += (n1 - i);
             arr[k++] = right[j++];
         }
     }
-    // add the remaining elements
     while(i < n1)
         arr[k++] = left[i++];
     while(j < n2)
@@ -37,11 +31,10 @@ int inversion_helper(vector<int> &arr, int start, int end) {
     if(start >= end)
         return 0;
     
-    // recursivec calls: merge sort
+    // recursivec  merge sort
     int result = 0, mid = start + (end - start) / 2;
     result += inversion_helper(arr, start, mid);
     result += inversion_helper(arr, mid + 1, end);
-    // cross-inversions
     result += merge(arr, start, mid, end);
     
     return result;
