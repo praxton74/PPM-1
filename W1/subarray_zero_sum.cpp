b6e5a72d00dd8a1b9f840f1485a1cfaf37e43430
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 16:56:53 +0530

    Update subarray_zero_sum.cpp

--- W1/subarray_zero_sum.cpp
+++ W1/subarray_zero_sum.cpp
@@ -1,19 +1,13 @@
-/*
-    Problem Link: https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1
-*/
-
-int maxLen(int arr[], int n) {
-    // hashmap of cumulative sum --> index
+int maxLen(int arr[], int n) 
+{
     unordered_map<int, int> m;
     int max_len = 0, cur_sum = 0;
-
     for(int i = 0; i < n; i++) {
         cur_sum += arr[i];
         if(cur_sum == 0) {
             max_len = i + 1;
             continue;
         }
-        // look for cur_sum in the hashmap
         if(m.find(cur_sum) != m.end()) 
             max_len = max(max_len, i - m[cur_sum]);
         else
