011f88c4afe4ee36ffd8aa44a1402439619f9d54
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 15:48:47 +0530

    Update Two_sum.cpp

--- W1/Two_sum.cpp
+++ W1/Two_sum.cpp
@@ -1,19 +1,13 @@
-/*
-    Problem Link: https://leetcode.com/problems/two-sum/
-*/
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> m {{nums[0], 0}}; // element, index of occurrence
+        unordered_map<int, int> m {{nums[0], 0}}; 
         int n = nums.size();
         for(int i = 1; i < n; i++) {
             int rem = target - nums[i];
             if(m.find(rem) != m.end()) {
-                // required element exists in the array so far
                 return {m[rem], i};
             }
-            // update the current element in the map
             m.insert({nums[i], i});
         }
         return {};
