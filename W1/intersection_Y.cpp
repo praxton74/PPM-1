0c24164eca508b6974ac55e7eceefe35a516f5ff
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 15:53:02 +0530

    Update intersection_Y.cpp

--- W1/intersection_Y.cpp
+++ W1/intersection_Y.cpp
@@ -1,6 +1,3 @@
-/*
-    Problem Link: https://leetcode.com/problems/intersection-of-two-linked-lists/
-*/
 
 ListNode *getIntersectionNode(ListNode *headA, Listnode *headB) {
     ListNode *a = headA;
