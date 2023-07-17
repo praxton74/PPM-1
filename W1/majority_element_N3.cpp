f057e8b043e485dc81608518a8bc46b11d7b82c2
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 16:06:49 +0530

    Update majority_element_N3.cpp

--- W1/majority_element_N3.cpp
+++ W1/majority_element_N3.cpp
@@ -1,10 +1,5 @@
-/*
-    Problem Link: https://leetcode.com/problems/majority-element-ii/
-*/
-
 class Solution {
 public:
-    // Boyer Moore Voting Algorithm
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> results;
         int num1 = -1, num2 = -1, count1 = 0, count2 = 0;
@@ -14,12 +9,10 @@ public:
             else if(x == num2)
                 count2++;
             else if(count1 == 0) {
-                // reset values
                 num1 = x;
                 count1 = 1;
             }
             else if(count2 == 0) {
-                // reset values
                 num2 = x;
                 count2 = 1;
             }
@@ -28,7 +21,6 @@ public:
                 count2--;
             }
         }
-        // process the results
         count1 = count2 = 0;
         int n = nums.size();
         for(int &x : nums) {
@@ -37,7 +29,6 @@ public:
             else if(x == num2)
                 count2++;
         }
-        // condition checks
         if(count1 > n / 3)
             results.push_back(num1);
         if(count2 > n / 3)
