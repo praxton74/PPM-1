87b66c056744f99c27b2567a237b02cf0f0c7bdc
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 17:43:12 +0530

    Update Four_sum.cpp

--- W1/Four_sum.cpp
+++ W1/Four_sum.cpp
@@ -1,35 +1,35 @@
-vector<vector<int>> fourSum(vector<int> &nums, int target) {
-    vector<vector<int>> result;
-    int n = nums.size();
-    if(nums.size() < 4)
-        return {};
-    sort(nums.begin(), nums.end());
-    for(int i = 0; i < n - 3; i++) 
-    {
-        if(i > 0 && nums[i] == nums[i - 1])
-            continue;
-        for(int j = i + 1; j < n - 2; j++) 
-        {
-            if(j > i + 1 && nums[j] == nums[j - 1])
-                continue;
-            int left = j + 1, right = n - 1;
-            while(left < right) 
-            {
-                int sum = nums[i] + nums[j] + nums[left] + nums[right];
-                if(sum == target) 
-                {
-                    result.push_back({nums[i], nums[j], nums[left], nums[right]});
-                    left++;
-                    while(left < right && nums[left] == nums[left - 1])
-                        left++;
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        if(n<4) return ans;
+        sort(begin(nums), end(nums));
+        for(int i=0;i<n;i++) {
+            long long nt1 = (long long) target - (long long)nums[i];
+            for(int j=i+1;j<n;j++) {
+                long long nt2 = nt1 - (long long)nums[j];
+                int s = j+1, e = n-1;
+                while(s<e){
+                    long long cur_sum = nums[s]+nums[e];
+                    if(cur_sum < nt2) s++;
+                    else if(cur_sum > nt2) e--;
+                    else{
+                        vector<int> q(4, 0);
+                        q[0] = nums[i];
+                        q[1] = nums[j];
+                        q[2] = nums[s];
+                        q[3] = nums[e];
+                        
+                        ans.push_back(q);
+                        while(s<e and nums[s]==q[2]) s++;
+                        while(s<e and nums[e]==q[3]) e--;
+                    }
                 }
-                else if(sum < target)
-                    left++;
-                else
-                    right--;
+                while(j+1 < n and nums[j+1]==nums[j]) ++j;
             }
+            while(i+1 < n and nums[i+1]==nums[i]) ++i;
         }
+        return ans;
     }
-    return result;
-}
-
+};
