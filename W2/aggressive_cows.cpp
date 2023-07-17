cc58de6b114fb3dc7f4434752b17de96782538e8
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 17:50:18 +0530

    Update aggressive_cows.cpp

--- W2/aggressive_cows.cpp
+++ W2/aggressive_cows.cpp
@@ -1,42 +1,30 @@
-bool isPossible(int *arr, int n, int c, int min_sep) {
-    int prev_cow = arr[0], count = 1;
-    for(int i = 1; i < n; i++) {
-        if(arr[i] - prev_cow >= min_sep) {
-            count++;
-            prev_cow = arr[i];
-            if(count == c)
-                return true;
+bool possible(int mid, vector<int> &stalls, int k){
+        int i, temp=stalls[0], count=1;
+        for(i=1; i<stalls.size(); i++){
+            if(stalls[i]-temp>=mid){
+                temp=stalls[i];
+                count++;
+            }
+            if(count==k)
+            return true;
         }
+        return false;
     }
-    return false;
-}
-int find(int *arr, int n, int c) {
-    int start = 0, end = arr[n - 1] - arr[0];
-    int max_sep = 0;
-
-    while(start <= end) {
-        int mid = start + (end - start) / 2;
-        if(isPossible(arr, n, c, mid)) {
-            max_sep = mid;
-            start = mid + 1;
+    int solve(int n, int k, vector<int> &stalls) {
+    
+        // Write your code here
+        sort(stalls.begin(), stalls.end());
+        int low=1, high=stalls[n-1]-stalls[0];
+        int ans=1;
+        while(low<=high){
+            int mid= low + (high-low)/2;
+            
+            if(possible(mid, stalls, k)){
+                low=mid+1;
+                ans=max(ans, mid);
+            }
+            else
+                high=mid-1;
         }
-        else    
-            end = mid - 1;
+        return ans;
     }
-    return max_sep;
-}
-
-int main() {
-
-    int T, n, c;
-    cin >> T;
-    while(T--) {
-        cin >> n >> c;
-        int arr[n];
-        for(int i = 0; i < n; i++)
-            cin >> arr[i];       
-        sort(arr, arr + n);
-        cout << find(arr, n, c) << endl;
-    }
-    return 0;
-}
