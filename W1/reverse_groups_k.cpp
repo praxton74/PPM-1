8253c67af30bebe82785362206560e98f90400ba
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 16:37:08 +0530

    Update reverse_groups_k.cpp

--- W1/reverse_groups_k.cpp
+++ W1/reverse_groups_k.cpp
@@ -1,7 +1,3 @@
-/*
-    Problem Link: https://leetcode.com/problems/reverse-nodes-in-k-group/
-*/
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -13,10 +9,8 @@
  * };
  */
 class Solution {
-    // temp head and tail
     ListNode *th = NULL, *tt = NULL;
 public:
-    // helper function to calculate the size of the linked list
     int length(ListNode *head) {
         int size = 0;
         while(head) {
@@ -25,8 +19,6 @@ public:
         }
         return size;
     }
-    
-    // helper function to add node at the starting of the list
     void addFirst(ListNode *node) {
         if(th == NULL)
             th = tt = node;
@@ -37,29 +29,19 @@ public:
     }
     
     ListNode* reverseKGroup(ListNode* head, int k) {
-        // edge cases
         if(!head || !head->next || k == 1)
             return head;
-        // find the length of the list
         int size = length(head);
         ListNode *curr = head;
-        
-        // original head and tails for the final merged linked list
         ListNode *oh = NULL, *ot = NULL;
-        
         while(size >= k) {
             int temp = k;
-            // reverse the links of K nodes
             while(temp--) {
-                // take the left most node
                 ListNode *N = curr->next;
-                // break the link
                 curr->next = NULL;
                 addFirst(curr);
-                // update values for next iteration
                 curr = N;
             }
-            // first set of k reversed nodes
             if(!oh) {
                 oh = th;
                 ot = tt;
@@ -69,12 +51,9 @@ public:
                 ot = tt;
             }
             
-            // reset th and tt
             th = tt = NULL;
-            // decrease size
             size -= k;
         }
-        // attach the remaining part: residual group of size < k 
         if(size)
             ot->next = curr;
         return oh;
