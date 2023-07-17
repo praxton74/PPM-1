fe4aa45675e68ce5d69f199f3c5fca4c546e0edf
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 17:59:49 +0530

    Update nth_root.cpp

--- W2/nth_root.cpp
+++ W2/nth_root.cpp
@@ -1,45 +1,24 @@
-/*
-    Find nth root of a number m upto p decimal places.
-*/
-
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-// multiply num n times
-double multiply(double num, int n) {
-    double ans = 1.0;
-    while(n--) 
-        ans *= num;
-    
-    return ans;
-}
-
-void getNthRoot(int n, int m, int p) {
-    double start = 1, end = m;
-    double eps = pow(10, -p);
-
-    while((end - start) > eps) {
-        double mid = start + (end - start) / 2;
-        if(multiply(mid, n) < m)
-            start = mid;
-        else
-            end = mid;
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+        long long m = n;
+        if(n < 0){
+            m = (-1) * m;
+        }
+        while(m > 0){
+            if(m % 2 == 1){
+                ans = ans * x;
+                m = m -1;
+            }
+            else{
+                x = x*x;
+                m = m/2;
+            }
+        }
+        if(n < 0){
+            ans = 1/ans;
+        }
+        return ans;
     }
-
-    cout << "estimated ans:";
-    cout << start << " " << end << endl;
-    cout << "actual ans:";
-    cout << pow(m, (double)(1.0 / (double)n));
-}
-
-int main() {
-
-    int n, m, p;
-    cin >> n >> m >> p;
-
-    getNthRoot(n, m, p);
-
-    return 0;
-}
-
+};
