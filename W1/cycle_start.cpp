d5710c18cbed0382cc319c073b301d3256804382
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 15:51:01 +0530

    Update cycle_start.cpp

--- W1/cycle_start.cpp
+++ W1/cycle_start.cpp
@@ -1,21 +1,15 @@
-/*
-    Problem Link: https://leetcode.com/problems/linked-list-cycle-ii/
-*/
-
-// return the node where the cycle begins
 ListNode *detectCycle(ListNode *head) {
     ListNode *slow = head;
     ListNode *fast = head;
 
     while(fast && fast->next) {
-        slow = slow->next; // 1 step
-        fast = fast->next->next; // 2 steps
-
-        // cycle detected
-        if(slow == fast) {
-            // move fast to the head
+        slow = slow->next; 
+        fast = fast->next->next; 
+        if(slow == fast) 
+        {
             fast = head;
-            while(slow != fast) {
+            while(slow != fast) 
+            {
                 slow = slow->next;
                 fast = fast->next;
             }
