2e3a5e3df60e0c86417b03751db0c5c78d9c1cc8
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 17:45:02 +0530

    Update flattening_list.cpp

--- W1/flattening_list.cpp
+++ W1/flattening_list.cpp
@@ -1,11 +1,8 @@
-
 Node *mergeLists(Node *l1, Node *l2) {
-    // base cases
     if(!l1)
         return l2;
     if(!l2)
         return l1;
-    // recursive calls
     if(l1->data <= l2->data) {
         l1->bottom = mergeLists(l1->bottom, l2);
         return l1;
@@ -17,9 +14,7 @@ Node *mergeLists(Node *l1, Node *l2) {
 }
 
 Node *flatten(Node *root) {
-    // base case
     if(!root || !root->next)
         return root;
-    // recursive call
     return mergeLists(root, flatten(root->next));
 }
