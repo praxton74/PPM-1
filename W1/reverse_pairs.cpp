1ba3b17fbc4e182c02483f03d6e438fd1a247eaf
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 16:38:19 +0530

    Update reverse_pairs.cpp

--- W1/reverse_pairs.cpp
+++ W1/reverse_pairs.cpp
@@ -1,17 +1,9 @@
-/*
-    Problem Link: https://leetcode.com/problems/reverse-pairs/
-*/
-
 class Solution {
 public:
-    // similar to merge sort: count inversions
-    
-    // helper function to count inversions
     int merge(vector<int> &nums, int start, int mid, int end) {
         int count = 0;
-        int n1 = mid - start + 1; // mid is included
+        int n1 = mid - start + 1;
         int n2 = end - mid;
-        // fill the values
         int left[n1], right[n2];
         
         for(int i = 0; i < n1; i++)
@@ -19,8 +11,6 @@ public:
         for(int i = 0; i < n2; i++)
             right[i] = nums[mid + i + 1];
         
-        // count the required pairs 
-        // traverse the left part of the array
         int j = 0;
         for(int i = 0; i < n1; i++) {
             while(j < n2 && left[i] > 2LL * right[j]) 
@@ -28,7 +18,6 @@ public:
             count += j;
         }
         
-        // 3 pointers
         int i = 0, k = start;
         j = 0;
         while(i < n1 && j < n2) {
@@ -37,7 +26,6 @@ public:
             else
                 nums[k++] = right[j++];
         }
-        // add the remaining elements
         while(i < n1)
             nums[k++] = left[i++];
         while(j < n2)
@@ -46,16 +34,13 @@ public:
         return count;
     }
     
-    // recursive mergeSort function
     int mergeSort(vector<int> &nums, int start, int end) {
-        // base case
         if(start >= end)
             return 0;
         
         int mid = start + (end - start) / 2;
         int count = mergeSort(nums, start, mid);
         count += mergeSort(nums, mid + 1, end);
-        // cross inversions
         count += merge(nums, start, mid, end);
         
         return count;
