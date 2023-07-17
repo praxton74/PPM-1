6a4392e3d1cbd662775e1a559052358ca897eb1e
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 16:35:35 +0530

    Update remove_nth_node.cpp

--- W1/remove_nth_node.cpp
+++ W1/remove_nth_node.cpp
@@ -1,7 +1,3 @@
-/*
-    Problem Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
-*/
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -16,19 +12,14 @@
 ListNode* removeNthFromEnd(ListNode *head, int n) {
     ListNode *slow = head;
     ListNode *fast = head;
-    // move fast pointer by n steps
     for(int i = 0; i < n; i++)
         fast = fast->next;
-    // edge cases: head node is to be deleted
     if(!fast)
         return head->next;
-    
-    // while fast reaches the last node
-    while(fast->next) {
+   while(fast->next) {
         slow = slow->next;
         fast = fast->next;
     }
-    // delete slow->next node
     slow->next = slow->next->next;
     return head;
 }
