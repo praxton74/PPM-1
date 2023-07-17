cca9210c120212ea4fddcaabcf99e33e6127fc80
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 15:52:51 +0530

    Update grid_unique_paths.cpp

--- W1/grid_unique_paths.cpp
+++ W1/grid_unique_paths.cpp
@@ -1,22 +1,15 @@
-/*
-    Problem Link: https://leetcode.com/problems/unique-paths/
-*/
-
-// Recursion with Memoization
-
 int countPaths(int sr, int sc, int dr, int dc, vector<vector<int>> &memo) {
         // base case: the bottom right destination is reached
         if(sr == dr && sc == dc)
             return 1;
         else if(sc > dc || sr > dr)
             return 0;
-        
-        // check for precomputed values
+    
         if(memo[sr][sc] != -1)
             return memo[sr][sc];
         // recursive calls
-        int h = countPaths(sr, sc + 1, dr, dc, memo); // horizontal move
-        int v = countPaths(sr + 1, sc, dr, dc, memo); // vertical move
+        int h = countPaths(sr, sc + 1, dr, dc, memo); 
+        int v = countPaths(sr + 1, sc, dr, dc, memo); 
         
         return memo[sr][sc] = h + v;
     }
@@ -27,13 +20,8 @@ int countPaths(int sr, int sc, int dr, int dc, vector<vector<int>> &memo) {
         return countPaths(1, 1, m, n, memo);
     }
 
-// Dynamic Programming
-
 int uniquePaths(int m, int n) {
-        // DP Solution
-        // dp[i][j]: number of unique paths to arrive at the point (i, j)
         vector<vector<int>> dp(m, vector<int>(n));
-        
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(i == 0 || j == 0)
