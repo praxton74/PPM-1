53fe7b1e40a2fb29eabac6d4e1b98abaaa317763
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-17 17:51:44 +0530

    Update allocate_min_pages.cpp

--- W2/allocate_min_pages.cpp
+++ W2/allocate_min_pages.cpp
@@ -1,79 +1,43 @@
-/*
-    Problem Link: https://practice.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1#
-*/
-#include<bits/stdc++.h>
-using namespace std;
-
-class Solution 
-{
-    public:
-    // helper function: greedy approach: give max possible load to each student
-    bool isPossible(int *arr, int n, int m, int cur_min) {
-        int students = 1;
-        int pages_read = 0;
-        
-        for(int i = 0; i < n; ) {
-            // assign pages to the current student
-            if(pages_read + arr[i] <= cur_min) {
-                pages_read += arr[i];
-                i++;
+bool isPossible(int maxPages, int A[], int M, int N){
+        int children = 1;
+        int pages = 0;
+        for(int i=0; i<N; i++){
+            if(pages + A[i] > maxPages){
+                children++;
+                pages = A[i];
             }
-            else {
-                // assign pages to the new student;
-                students++;
-                pages_read = 0;
-                // condition check
-                if(students > m)
-                    return false;
+            else{
+                pages += A[i];
             }
         }
-        return true;
+        if(children <= M){
+            return true;
+        }
+        return false;
     }
-
-    int findPages(int arr[], int n, int m) {
-        //code here
-        // edge case: students are more than the number of books
-        if(m > n)
+    int findPages(int A[], int N, int M) 
+    {
+        if(M > N){
             return -1;
-        
-        int result = INT_MAX;
-        // count the total number of pages
+        }
         int sum = 0;
-        for(int i = 0; i < n; i++) 
-            sum += arr[i];
-        
-        // apply binary search
-        int start = *max_element(arr, arr + n);
-        int end = sum;
-        
-        while(start <= end) {
-            int mid = start + (end - start) / 2;
-            // each student reads atmost mid number of pages [max upper_bound]
-            if(isPossible(arr, n, m, mid)) {
-                result = min(result, mid);
-                end = mid - 1;
+        int maxi = INT_MIN;
+        for(int i=0; i<N; i++){
+            sum += A[i];
+            maxi = max(maxi, A[i]);
+        }
+        int i = maxi;
+        int j = sum;
+        int ans = -1;
+        while(i <= j){
+            int mid = i + (j-i)/2;
+            if(isPossible(mid, A, M, N)){
+                ans = mid;
+                j = mid-1;
             }
-            else
-                start = mid + 1;
+            else{
+                i = mid+1;
+            }
         }
-        return result;
+        return ans;
     }
-};
-
-int main() {
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int A[n];
-        for(int i=0;i<n;i++){
-            cin>>A[i];
-        }
-        int m;
-        cin>>m;
-        Solution ob;
-        cout << ob.findPages(A, n, m) << endl;
-    }
-    return 0;
-}
