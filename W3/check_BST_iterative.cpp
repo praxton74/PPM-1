1038be526ce1c80f0c4a58441120d2f2d099432f
Parth <94388365+praxton74@users.noreply.github.com>
2023-08-14 14:19:38 +0530

    Update check_BST_iterative.cpp

--- W3/check_BST_iterative.cpp
+++ W3/check_BST_iterative.cpp
@@ -1,8 +1,3 @@
-/*
-    Problem Link: https://leetcode.com/problems/validate-binary-search-tree/
-*/
-
-// iterative inorder traversal
 bool isValidBST(TreeNode *root) {
     // edge case
     if(!root)
@@ -11,20 +6,15 @@ bool isValidBST(TreeNode *root) {
     TreeNode *prev = NULL;
 
     while(!s.empty() || root) {
-        // push the nodes on the left
         while(root) {
             s.push(root);
             root = root->left;
         }
-        // leftmost node is taken into consideration
         root = s.top();
         s.pop();
-        // violation of increasing order condition
         if(prev && root->val <= prev->val)
             return false;
-        // update prev to be the current node under consideration
         prev = root;
-        // move to the right child
         root = root->right;
     }
     return true;
