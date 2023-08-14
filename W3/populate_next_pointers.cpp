8568523c74e0112a5bd3e687539a211a8c8b978e
Parth <94388365+praxton74@users.noreply.github.com>
2023-08-14 14:28:01 +0530

    Update populate_next_pointers.cpp

--- W3/populate_next_pointers.cpp
+++ W3/populate_next_pointers.cpp
@@ -1,7 +1,3 @@
-/*
-    Problem Link: https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
-*/
-
 Node *connect(Node *root) {
     // edge case
     if(!root)
@@ -10,19 +6,15 @@ Node *connect(Node *root) {
     queue<Node*> q;
     q.push(root);
     Node *curr;
-    // perform BFS: level order traversal
     while(!q.empty()) {
-        int n = q.size(); // denotes the number of nodes in the current level
+        int n = q.size(); 
         for(int i = 0; i < n; i++) {
             curr = q.front();
             q.pop();
-            // make the last node point to NULL: no node on its right
             if(i == n - 1)
                 curr->next = NULL;
-            // make the current node point to the next node in the queue
             else    
                 curr->next = q.front();
-            // add the children in the queue if they exist
             if(curr->left)
                 q.push(curr->left);
             if(curr->right)
