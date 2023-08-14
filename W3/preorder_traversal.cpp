7153a95737312c6b9c46e6a7c839158f7b09caf5
Parth <94388365+praxton74@users.noreply.github.com>
2023-08-14 14:28:35 +0530

    Update preorder_traversal.cpp

--- W3/preorder_traversal.cpp
+++ W3/preorder_traversal.cpp
@@ -1,7 +1,3 @@
-/*
-    Problem Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
-*/
-
 vector<int> preorderTraversal(TreeNode *root) {
     // base case
     if(!root)
@@ -9,17 +5,12 @@ vector<int> preorderTraversal(TreeNode *root) {
     
     vector<int> results;
     stack<TreeNode*> s;
-    // NLR: so the root node is printed first
     s.push(root);
 
-    // we follow level order traversal
     while(!s.empty()) {
         TreeNode *front = s.top();
         s.pop();
-        // add the data in the results
         results.push_back(front->val);
-        // add the right child first before the left one
-        // owing to LIFO rule, the left child will be popped first
         if(front->right)
             s.push(front->right);
         if(front->left)
