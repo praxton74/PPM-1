4715f41a563789e2a236eb0e5438577dd178eae2
Parth <94388365+praxton74@users.noreply.github.com>
2023-08-14 14:28:18 +0530

    Update postorder_traversal.cpp

--- W3/postorder_traversal.cpp
+++ W3/postorder_traversal.cpp
@@ -1,7 +1,3 @@
-/*
-    Problem Link: https://leetcode.com/problems/binary-tree-postorder-traversal/
-*/
-
 vector<int> postorderTraversal(TreeNode *root) {
     // base case
     if(!root)
@@ -10,13 +6,9 @@ vector<int> postorderTraversal(TreeNode *root) {
     vector<int> results;
     stack<TreeNode*> s;
     s.push(root);
-    // LRN: perform level order traversal
     while(!s.empty()) {
         TreeNode *front = s.top();
-        // add the data in the results
         results.push_back(front->val);
-        // NRL (reverse of LRN) is performed
-        // owing to LIFO rule, we push the left child first
         if(front->left)
             s.push(front->left);
         if(front->right)
