69edd4d3027793fa35a3bb2ce8de7a5b53ece8ef
Parth <94388365+praxton74@users.noreply.github.com>
2023-08-14 14:29:10 +0530

    Update right_view.cpp

--- W3/right_view.cpp
+++ W3/right_view.cpp
@@ -1,18 +1,12 @@
-/*
-    Problem Link: https://leetcode.com/problems/binary-tree-right-side-view/
-*/
-
 void findRightView(TreeNode* root, int level, int &max_level, vector<int> &results) {
     // base case
     if(!root) 
         return;
-    // new node (rightmost) of each new level
     if(level > max_level) {
         results.push_back(root->val);
         max_level = level;
     }
     
-    // recursive calls: right subtree first
     findRightView(root->right, level + 1, max_level, results);
     findRightView(root->left, level + 1, max_level, results);
 }
