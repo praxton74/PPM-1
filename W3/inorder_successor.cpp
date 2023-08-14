704cb74c7a6a05e09c24619a00cd4d77f4618b88
Parth <94388365+praxton74@users.noreply.github.com>
2023-08-14 14:22:55 +0530

    Update inorder_successor.cpp

--- W3/inorder_successor.cpp
+++ W3/inorder_successor.cpp
@@ -1,8 +1,3 @@
-/*
-    Problem Link: https://practice.geeksforgeeks.org/problems/inorder-successor-in-bst/1
-*/
-
-// successor has a value just greater than the node x
 Node *inOrderSuccessor(Node *root, int x) {
     Node *succ = NULL;
     while(root) {
