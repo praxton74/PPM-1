6b122140bcafb7a5c8078da955e1d46c2dc8bf58
Parth <94388365+praxton74@users.noreply.github.com>
2023-08-14 14:28:56 +0530

    Update reverse_words.cpp

--- W3/reverse_words.cpp
+++ W3/reverse_words.cpp
@@ -1,7 +1,3 @@
-/*
-    Problem Link: https://leetcode.com/problems/reverse-words-in-a-string/
-*/
-
 class Solution {
 public:
     string reverseWords(string s) {
@@ -9,19 +5,15 @@ public:
         string word;
         int n = s.size();
         for(int i = 0; i < n; i++) {
-            // leading spaces at the start
             if(word.empty()) {
                 while(s[i] == ' ')
                     i++;
             }
-            // add the characters to the string word
             if(s[i] != ' ')
                 word += s[i];
-            // push the words when space is encountered or i == (n - 1), for the last word
             if(s[i] == ' ' || i == n - 1) {
                 if(!word.empty())
                     st.push(word);
-                // reset string to store the next word
                 word.clear();
             }
         }
@@ -31,7 +23,6 @@ public:
             st.pop();
             word += " ";
         }
-        // remove the last added space 
         word.pop_back();
         return word;
     }
